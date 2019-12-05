1af8f8437f31be027d29809302f3bdfccb20da52
Matthias Steffen <matthias@element.in>
2019-12-05 11:56:10 +0100

    Completed first module of the workshop.

--- src/resolvers.js
+++ src/resolvers.js
@@ -1,6 +1,26 @@
+import data from '../data'
+
 const resolvers = {
   Query: {
-    hello: () => `Hello World!`
+    hello: () => `Hello World!`,
+    articles: (_, {searchText}) => {
+      if(searchText){
+        return data.articles.filter(article => article.title.toLocaleLowerCase().includes(searchText.toLocaleLowerCase()))
+      }
+      return data.articles
+    },
+    users: (_, { limit }) => {
+      if(limit) {
+        return data.users.slice(0, limit)
+      }
+      return data.users
+    },
+    article: (_, { id }) => {
+      return data.articles.filter(article => article.id === id)[0]
+    },
+    user: (_, { id }) => {
+      return data.users.filter(user => user.id === id)[0]
+    }
   }
 };
 
