a054b40591be732a246bf68f73ae30994e912d36
Matthias Steffen <matthias@element.in>
2019-12-05 12:04:15 +0100

    Extracted resolver functions.

--- src/resolvers.js
+++ src/resolvers.js
@@ -1,26 +1,35 @@
 import data from '../data'
 
+function getArticles(searchText) {
+  if (searchText) {
+    return data.articles.filter(article => article.title.toLocaleLowerCase().includes(searchText.toLocaleLowerCase()));
+  }
+  return data.articles;
+}
+
+function getUsers(limit) {
+  if (limit) {
+    return data.users.slice(0, limit)
+  }
+  return data.users
+}
+
+function findArticleById(id) {
+  return data.articles.filter(article => article.id === id)[0]
+}
+
+function findUserById(id) {
+  return data.users.filter(user => user.id === id)[0]
+
+}
+
 const resolvers = {
   Query: {
     hello: () => `Hello World!`,
-    articles: (_, {searchText}) => {
-      if(searchText){
-        return data.articles.filter(article => article.title.toLocaleLowerCase().includes(searchText.toLocaleLowerCase()))
-      }
-      return data.articles
-    },
-    users: (_, { limit }) => {
-      if(limit) {
-        return data.users.slice(0, limit)
-      }
-      return data.users
-    },
-    article: (_, { id }) => {
-      return data.articles.filter(article => article.id === id)[0]
-    },
-    user: (_, { id }) => {
-      return data.users.filter(user => user.id === id)[0]
-    }
+    articles: (_, { searchText }) => getArticles(searchText),
+    users: (_, { limit }) => getUsers(limit),
+    article: (_, { id }) => findArticleById(id),
+    user: (_, { id }) => findUserById(id)
   }
 };
 
